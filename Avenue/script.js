5f8e74debecfe06ee0a3feb7c842f409ca41c62a
mjzart <serg.dorosh15@gmail.com>
2020-10-28 23:08:45 +0200

    Avenue28.10

--- Avenue/script.js
+++ Avenue/script.js
@@ -1,9 +1,22 @@
 const imgItem = document.querySelectorAll('.img-item')
+const item = document.querySelectorAll('.item')
 const imgItemFront = document.querySelectorAll('.mini-img-front')
 const imgItemBack = document.querySelectorAll('.mini-img-back')
 const price= document.querySelectorAll('.price-num')
 const description= document.querySelectorAll('.description')
 const priceDisc= document.querySelectorAll('.price-disc')
+
+fetch('https://my-json-server.typicode.com/Kolland/fe-basics-resources/categories',{
+    method:'GET'
+})
+    .then(response =>response.json())
+    .then(data => {
+        const filtersLielement = data.map(category => {
+            return `<li class="filter-item" data-filter=".${category}">${category}</li>`
+        })
+        $('.filter-box').append(filtersLielement.join(''))
+    })
+
 
 
 
@@ -35,4 +48,17 @@ fetch('https://my-json-server.typicode.com/Kolland/fe-basics-resources/products'
         description.forEach((el, index) =>{
             el.innerHTML = data[index]["description"]
         })
+        item.forEach((el, index)=>{
+            data[index]["categories"].forEach(category=>{
+                el.classList.add(category)
+            })
+        })
+
+        const $isotope = $('.item').isotope();
+        $('.filter-item').on('click', function() {
+        console.log(this.dataset.filter);
+        $isotope.isotope({ filter: this.dataset.filter });
     })
+    })
+
+    
